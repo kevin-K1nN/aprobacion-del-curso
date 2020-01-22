be60006b5d0c573b71b0a3dc228474e3caa5662e
kevin-K1nN <ellisto_9@hotmail.com>
2020-01-22 00:19:37 -0300

    aprobacion del curso

---
+++ PHP/partials/header.php
@@ -0,0 +1,45 @@
+<?php
+  require_once('controladores/funciones.php');
+  require_once('helpers.php');
+?>
+<nav class="d-flex navbar navbar-expand-lg navbar-dark __nave">
+          <div class="d-flex flex-grow-1">
+              <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
+              <div class="navbar-brand" href="#">
+                  <a href="index.php"><img class="__imglogo" src="https://www.digitalhouse.com/logo-DH.png" alt="logo de Digital House" width="300"></a>
+              </div>
+              <div class="w-100 text-right">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+              </div>
+          </div>
+
+          <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
+              <ul class="navbar-nav ml-auto flex-nowrap __navbar">
+                  <li class="nav-item o_navitem">
+                      <a href="#estrenos" class="nav-link">Próximos Estrenos</a>
+                  </li>
+                  <li class="nav-item o_navitem">
+                      <a href="#" class="nav-link">Publicidad  Eventos</a>
+                  </li>
+                  <!--Esto lo estoy colocando para controlar que debe aparecer en las opciones del navbar, dependiendo si el usuario está logueado o no-->
+
+                  <?php if(!isset($_SESSION["email"])) :?>
+                        <li class="nav-item o_navitem">
+                            <a href="login.php" class="nav-link">Logín</a>
+                        </li>
+                        <li class="nav-item o_navitem">
+                            <a href="register.php" class="nav-link">Registro</a>
+                        </li>
+                  <?php else :?>
+
+                        <li class="nav-item o_navitem">
+                        <a href="logout.php" class="nav-link">Cerrar Sesión </a>
+                      </li>
+
+                  <?php endif;?>
+              </ul>
+
+          </div>
+        </nav>
